4118bb3c86e1b328e93167f5dc2bf2dce4ebc35e
Ash Searle <ash@hexmen.com>
2020-12-22 11:26:25 +0000

    Fix docker config and postgres init for macOS

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE ROLE notesadmin WITH LOGIN PASSWORD 'password';
+    ALTER ROLE notesadmin WITH LOGIN PASSWORD 'password';
     ALTER ROLE notesadmin WITH SUPERUSER;
     ALTER DATABASE notesapi OWNER TO notesadmin;
 
